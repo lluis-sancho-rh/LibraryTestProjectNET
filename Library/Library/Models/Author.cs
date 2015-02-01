ba0f07dbe965b4e31309a0547b336c047b365926
lluis-sancho-rh <luis.sancho@razorfishhealthware.com>
2015-02-01 19:30:57 +0100

    first upload

---
+++ Library/Library/Models/Author.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Library.Models
+{
+   public class Author
+    {
+
+        public int AuthorID { get; set; }
+
+        public string AuthorName { get; set; }
+
+        public virtual List<Book> BookList { get; set; }
+    }
+}
---
+++ Library/Library/Models/Book.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Library.Models
+{
+    public class Book
+    {
+
+        public int BookID { get; set; }
+
+        public string Title { get; set; }
+
+        public int NumPages { get; set; }
+
+        public Author Author { get; set; }
+
+
+        public double Price { get; set; }
+    }
+}
